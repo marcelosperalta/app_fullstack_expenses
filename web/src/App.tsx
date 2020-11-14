b3a5ade7a11d9a3c972c2d110c2c03c2e2569bbb
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2020-11-14 17:38:03 +0100

    expenses

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Login from "./pages/login"
 
+import './styles/global.css';
+
 function App() {
   return (
     <Login />
--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -1,7 +1,50 @@
 import React from "react";
 
+import "./style.css"
+
+import img from "../images/login-logo.png"
+
 export default function Login(){
     return (
-        <div>Login</div>
+        <div className="login">
+            <div className="image">
+                <img 
+                    src={img} 
+                    alt="Login"
+                />
+            </div>
+            <div className="title">
+                <p>Go Rich</p>
+            </div>
+            <div>
+                <input 
+                    type="text" 
+                    name="name" 
+                    id="name"
+                    placeholder="Username"
+                />
+            </div>
+            <div>
+                <input 
+                    type="password" 
+                    name="password" 
+                    id="password"
+                    placeholder="Password"
+                />
+            </div>
+            <div className="login-button">
+                <button 
+                    type="submit"
+                >
+                    Login
+                </button>
+            </div>
+            <div className="register-link">
+                Register
+            </div>
+            <div className="footer">
+                The best expense control app
+            </div>            
+        </div>
     )
 }
