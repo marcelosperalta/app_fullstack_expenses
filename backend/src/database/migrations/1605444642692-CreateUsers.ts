e876aa8132e6417b1e7c6c950b6d869728b2e7ee
bmodepri <garcia.priscila01@gmail.com>
2020-11-15 19:57:49 +0100

    expenses

---
+++ backend/src/database/migrations/1605444642692-CreateUsers.ts
@@ -0,0 +1,64 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1605444642692 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [{
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()',
+                }, 
+                {
+                    name: 'name',
+                    type: 'varchar',
+                },
+                {
+                    name: 'surname',
+                    type: 'varchar',
+                },
+                {
+                    name: 'username',
+                    type: 'varchar',
+                },
+                {
+                    name: 'email',
+                    type: 'varchar',
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
+                },
+                {
+                    name: 'createdAt',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+                {
+                    name: 'createdBy',
+                    type: 'numeric',
+                },
+                {
+                    name: 'lastUpdatedAt',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+                {
+                    name: 'lastUpdatedBy',
+                    type: 'numeric',
+                }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
--- push.ps1
+++ push.ps1
@@ -1,3 +1,5 @@
+git config user.name "bmodepri"
+git config user.email "garcia.priscila01@gmail.com"
 git add .
 git commit -m "expenses"
 git push
