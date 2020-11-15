64d5ced81bd293267b91b1186a58766d08a67b6b
Marcelo Soares Peralta <marcelosperalta@gmail.com>
2020-11-15 20:11:37 +0100

    push.ps1 removed

--- push.ps1
+++
@@ -1,5 +0,0 @@
-git config user.name "bmodepri"
-git config user.email "garcia.priscila01@gmail.com"
-git add .
-git commit -m "expenses"
-git push
--- push.sh
+++
@@ -1,3 +0,0 @@
-git add . 
-git commit -m "expenses"
-git push
