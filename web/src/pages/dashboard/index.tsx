af838921d3330029518dd5b5348d4c831839d2ab
marcelosperalta <marcelosperalta@gmail.com>
2020-12-16 16:27:58 +0100

    expenses

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -14,14 +14,56 @@ export default function Dashboard(){
                     Max Mustermann
                 </h1>
             </div>
+
             <div>
                 Week 35
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>            
+            <div>
+                Planned budget:
+            </div>
+            <div>
+                150.00 €
+            </div>
+            <div>
+                Remaining budget:
+            </div>
+            <div>
+                -32.00 €
+            </div>
+            <hr/>
+            <div>
+                Expenses
+            </div>
+            <div>
+                Budget
+            </div>
+
+            <div>
+                August
+            </div>
+            <div>
+                Planned budget:
+            </div>
+            <div>
+                1850.00 €
+            </div>
+            <div>
+                Remaining budget:
+            </div>
+            <div>
+                432.00 €
+            </div>
+            <hr/>
+            <div>
+                Expenses
+            </div>
+            <div>
+                Budget
+            </div>
+
+            <div>
+                View Reports
+            </div>
         </div>
     )
 }
